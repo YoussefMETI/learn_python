3e44f74309096233d21f05f33d1d23d8583659ab
YoussefMETI <youssef.saad@meti.dev>
2024-12-31 12:49:05 +0200

    circle

---
+++ Circle.py
@@ -0,0 +1,8 @@
+import math
+
+radius = int(input("Enter radius :"));
+
+circumference = 2*radius*math.pi;
+area =  2*radius*math.pi;
+print("circumference : ",circumference);
+print("area : ",area);
