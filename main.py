feb938e7a79d13365b4dff6dec45eb0c5775c94c
YoussefMETI <youssef.saad@meti.dev>
2024-12-31 12:18:37 +0200

    calculator

---
+++ main.py
@@ -0,0 +1,6 @@
+first_num = input("Enter your first number :");
+second_num = input("Enter your second number :");
+print("Sum :", int(first_num) + int(second_num));
+print("division :", int(first_num) / int(second_num));
+print("sub :", int(first_num) - int(second_num));
+print("muliplication :", int(first_num) * int(second_num));
